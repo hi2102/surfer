e1732c54dfa81075d0a9c6da8ba3708ecfbd2289
FOREVER-YOUNG <brilliant2102@gmail.com>
2021-10-07 12:57:12 +0900

    2021년10월07일수업커밋

---
+++ app.js
@@ -0,0 +1,35 @@
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.seaWidth = document.body.clientWidth;
+    this.seaHeight = document.body.clientHeight;
+
+    this.canvas.width = this.seaWidth * 2;
+    this.canvas.height = this.seaheight * 2;
+    this.ctx.scale(2, 2);
+  }
+
+  animate(t) {
+    requestAnimationFrame(this.animate.bind(this));
+    this.ctx.clearRect(0, 0, this.seaWidth, this.seaHeight);
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(this.seaWidth/2, this.seaHeight /2 , this.seaWidth + this.seaWidth/6, this.seaHeight / 2 + this.seaHeight / 6);
+  }
+
+}
+
+window.onload = () => {
+  new App();
+};
+
+// resize
